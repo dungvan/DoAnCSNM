2039f5ea6b220a649f62b9f26f12bcaf444e4c73
dungvan <1dungvan2512@gmail.com>
2017-05-15 18:14:57 +0700

    v13.0

--- CSNM/HDH/C++_Project/DLL/DLL.h
+++
@@ -1,5 +0,0 @@
-﻿#include <Windows.h>
-#include <fstream>
-
-__declspec(dllexport) LRESULT __stdcall CALLBACK FillKeyboard(int nCode, WPARAM wParam, LPARAM lParam);
-__declspec(dllexport) BOOL InstallHook();
